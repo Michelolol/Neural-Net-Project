b3f6dfaac83bb4ce8c8933678ba51844d060d365
Michelolol <bluedragonboy888@gmail.com>
2023-04-25 09:14:17 -0500

    a

--- .Michael-Work.py
+++ .Michael-Work.py
@@ -1,6 +1,6 @@
 from neural import NeuralNet
 #what we want to use- estimate per 100 civilians, # registered, # unregistered, gun death rate, ,Suicide Rate by Firearm,Unintentional Deaths by Firearms, Rate Police Killing (per 10M)
-#  ([Gp100Civ, GReg, GUnReg, GDeathRate, GSucideRate, GUninentDeath, GRatePolice], [RestrictVs!Restrict GLaws]),
+#  ([Gp100Civ, GReg, GUnReg, GDeathRate, GSucideRate, GUninentDeath, GRatePolice], [RestrictVs!Restrict GLaws (1, 0)]),
 
 
 
