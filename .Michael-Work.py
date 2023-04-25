251558080ac8e56d39a7469cb94ff2fade4a09ff
Michelolol <bluedragonboy888@gmail.com>
2023-04-25 09:17:06 -0500

    xorn added

--- .Michael-Work.py
+++ .Michael-Work.py
@@ -2,6 +2,7 @@ from neural import NeuralNet
 #what we want to use- estimate per 100 civilians, # registered, # unregistered, gun death rate, ,Suicide Rate by Firearm,Unintentional Deaths by Firearms, Rate Police Killing (per 10M)
 #  ([Gp100Civ, GReg, GUnReg, GDeathRate, GSucideRate, GUninentDeath, GRatePolice], [RestrictVs!Restrict GLaws (1, 0)]),
 
+xorn = NeuralNet(7, 6, 1)
 
 
 
