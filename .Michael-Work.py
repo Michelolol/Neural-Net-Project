79f7a2caf44ad63a203880900968473e6042cf52
Michelolol <bluedragonboy888@gmail.com>
2023-04-25 09:05:39 -0500

    aaaaaaaaaaaaa

--- .Michael-Work.py
+++ .Michael-Work.py
@@ -1,5 +1,5 @@
 from neural import NeuralNet
-#what we want to use- estimate per civilians, # registered, # unregistered, gun death rate
+#what we want to use- estimate per 100 civilians, # registered, # unregistered, gun death rate, ,Suicide Rate by Firearm,Unintentional Deaths by Firearms, Rate Police Killing (per 10M)
 
 
 
