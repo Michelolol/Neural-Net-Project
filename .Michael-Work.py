f73a988ba0ec3338556fc2eea89d383f645dbb72
Michelolol <bluedragonboy888@gmail.com>
2023-04-27 09:14:01 -0500

    billy fuck

--- .Michael-Work.py
+++ .Michael-Work.py
@@ -47,7 +47,7 @@ training_data = [
 
 
 
-billy = NeuralNet(7, 6, 1)
+billy = NeuralNet(7, 7, 1)
 billy.train(training_data)
 
 #[Gp100Civ, GReg, GUnReg, GDeathRate, GSucideRate, GUninentDeath, GRatePolice],
