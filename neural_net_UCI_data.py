81594353e7997e1f8f5d264a6bd4ea8947689c3a
Michelolol <bluedragonboy888@gmail.com>
2023-05-05 09:15:19 -0500

    checks

--- neural_net_UCI_data.py
+++ neural_net_UCI_data.py
@@ -5,6 +5,7 @@ from neural import *
 def parse_line(line: str) -> Tuple[List[float], List[float]]:
     """Splits line of CSV into inputs and output (transormfing output as appropriate)
 
+    
     Args:
         line - one line of the CSV as a string
 
